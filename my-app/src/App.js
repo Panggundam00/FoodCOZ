68f7922f56fa0791d2a80130f9d0f2a11ea95589
Thanakit Yannam <thanakit.ya@ku.th>
2019-07-02 08:21:01 +0700

    add managePage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
+import ManagePage from './nextPage/ManagePage'
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <lebal>FoodCOZ</lebal>
+      </div>
+
+      <label>ID</label>
+
+      <div>
+        <input/>
+      </div>
+
+      <div>
+        <lebal>Password</lebal>
+      </div>
+
+      <div>
+        <input/>
+      </div>
+
+      <button><a href="/ManagePage">Submit</a></button>
     </div>
   );
 }
---
+++ my-app/src/nextPage/ManagePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ManagePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>this is ManagePage</h1>
+            </div>
+        )
+    }
+}
+
+export default ManagePage
