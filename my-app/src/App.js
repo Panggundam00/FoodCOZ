561dbbcc57180eebafae713a1f9d8e4e8294567e
Woraton Sartsangchan <woraton.s@ku.th>
2019-07-02 10:47:49 +0700

    4771 files consist of node-module/my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+  render(){
+    return (
     <div className="App">
-      
+      <div>
+        <lebal>FoodCOZ</lebal>
+      </div>
+
+      <label>ID</label>
+
+      <div>
+        <input/>
+      </div>
+
+      <div>
+        <lebal>Password</lebal>
+      </div>
+
+      <div>
+        <input/>
+      </div>
+
+      <button><a href="/ManagePage">Submit</a></button>
     </div>
-  );
+
+    );
+  }
+
 }
 
 export default App;
