909aaf52dfcb9335fc1be078dc23417c64c5fc48
Thanakit Yannam <thanakit.ya@ku.th>
2019-07-02 13:06:26 +0700

    add login.js/managePage

--- my-app/src/ManagePage.js
+++ my-app/src/ManagePage.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import './App.css';
 
 class ManagePage extends Component {
     render() {
         return (
-            <div>
-                <h1>this is ManagePage</h1>
+            <div className="App">
+                <h1>Manage Store</h1>
+
+                <div>
+                    <button><a>Set table</a></button>
+                </div>
+
+                <div>
+                    <button><a>Set menu</a></button>
+                </div>
             </div>
         )
     }
